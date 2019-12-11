629aeda8593583cb02c1fbebe49c2a0500507fc8
Mohamed Salman <salman@vhitechlab.com>
2019-12-11 19:52:00 +0530

    salman | added basic setup and written basic get hello world api

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("python testing 123")
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "hello world"
+
+if __name__ == "__main__":
+    app.run()
+
+
